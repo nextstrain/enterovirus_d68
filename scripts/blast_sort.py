440b61fb4ea8fbf174f1d1ca0a5ace17437ecf81
Emma Hodcroft <emmahodcroft@gmail.com>
2023-03-02 12:15:48 +0100

    turn on/off dup check & write out no-hit seqs

--- scripts/blast_sort.py
+++ scripts/blast_sort.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -15,6 +16,11 @@ if __name__ == '__main__':
     parser.add_argument('--out_seqs', help="output seqs")
     parser.add_argument('--out_meta', help="output meta")
     parser.add_argument('--match_length', type=int, help="min number of bases to match")
+    parser.add_argument('--check_duplicates', action='store_true', help="whether to check duplicates, default true")
+    parser.add_argument('--no_hit_seqs', help="file containing list of sequences that had no blast hit")
+    parser.add_argument('--no-check_duplicates', dest='check_duplicates', action='store_false')
+    parser.add_argument('--not_matched_seqs', help="file containing a list of sequences not meeting minimum match length")
+    parser.set_defaults(feature=True)
     parser.add_argument('--dup_file', help="file to write duplicate info to")
     args = parser.parse_args()
 
@@ -22,7 +28,9 @@ if __name__ == '__main__':
     blast = pd.read_csv(args.blast, index_col=False,
         names=["qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore", "qcovs"])
 
-    #read in sequences
+    #read in sequences, store names
+    fasta_sequences = SeqIO.parse(open(args.seqs), 'fasta')
+    seq_names = [fasta.id for fasta in fasta_sequences]
     fasta_sequences = SeqIO.parse(open(args.seqs), 'fasta')
 
     # OLD: pick the ones we want - matches of at least 700 bp. 
@@ -31,17 +39,32 @@ if __name__ == '__main__':
     #Now allow users to specify match length...
     matchLen = args.match_length
 
+    no_keep_blast = set(list(blast.loc[blast.send - blast.sstart < matchLen, "qseqid"]))
+
     keep_blast = list(blast.loc[blast.send - blast.sstart >= matchLen, "qseqid"])
 
     blast['matchlength'] = blast.apply(lambda row: row.send - row.sstart, axis=1)
+
+    blasted_ids = set(blast.qseqid)
+    no_vp_match = [s for s in seq_names if s not in blasted_ids]
 
     print("{} of {} sequences had some kind of BLAST hit in VP1.".format(len(set(blast.qseqid)), len(list(fasta_sequences))))
     print("{} of the {} sequences have at least {}bp in VP1 and will be included".format(len(keep_blast), 
         len(set(blast.qseqid)),matchLen))
 
+    if args.no_hit_seqs:
+        pd.DataFrame(no_vp_match).to_csv(args.no_hit_seqs, sep='\t', index=False, header=False)
+
+    if args.not_matched_seqs:
+        pd.DataFrame(no_keep_blast).to_csv(args.not_matched_seqs, sep='\t', index=False, header=False)
+
     # These meta strain names are still uncorrected for forbidden chars, but the sequence names have been corrected...
     # so we need to make them comparable by removing the forbidden chars before we can find them in the meta...
-    meta = pd.read_csv(args.meta, sep='\t', index_col=False)
+    sepr = ","
+    if re.search('.tsv$', args.meta):
+        sepr = "\t"
+    
+    meta = pd.read_csv(args.meta, sep=sepr, index_col=False)
     keep_meta_forbid_names = []
     for f in meta.strain:
         name = f
@@ -60,32 +83,35 @@ if __name__ == '__main__':
     #the 'matchlength' will only be for the first match...
     #look for two-match-hits & remove
     dups = meta_matchLen.duplicated(subset='strain', keep='first')
+    unique_dup_hits = set(meta_matchLen[dups]['strain'])
     if sum(dups) != 0:
         print("\nRemoving {} two-match-hits. Note that two-match 'matchlength's will be only for the FIRST match.".format(sum(dups)))
+        print(f"A unique list of the {len(unique_dup_hits)} strains is: {unique_dup_hits}")
         print("Here are the accessions:")
         print(meta_matchLen[dups].accession)
         meta_matchLen = meta_matchLen[~dups]
 
-    #Need to remove duplicates - sequences with same strain that were sequenced twice.
-    #Some debug if want to look into duplicates:
-        #   dups = meta_matchLen.duplicated(subset="orig_strain", keep=False)
-        #   print(meta_matchLen.loc[dups, ['accession','orig_strain','matchlength']].to_string())
-    #First get all of them, to write out, for debugging/checking
-    dups2 = meta_matchLen.duplicated(subset="orig_strain", keep=False)
-    if not dups2.empty:
-        meta_matchLen[dups2].to_csv(args.dup_file, sep='\t', index=False)
+    if args.check_duplicates:
+        #Need to remove duplicates - sequences with same strain that were sequenced twice.
+        #Some debug if want to look into duplicates:
+            #   dups = meta_matchLen.duplicated(subset="orig_strain", keep=False)
+            #   print(meta_matchLen.loc[dups, ['accession','orig_strain','matchlength']].to_string())
+        #First get all of them, to write out, for debugging/checking
+        dups2 = meta_matchLen.duplicated(subset="orig_strain", keep=False)
+        if not dups2.empty:
+            meta_matchLen[dups2].to_csv(args.dup_file, sep='\t', index=False)
 
-    dups2 = meta_matchLen.duplicated(subset="orig_strain", keep="first")
-    dup_strains = list(meta_matchLen.loc[dups2,'orig_strain'])
-    for dup_s in dup_strains:
-        #sort by matchlength (longest at top), take first
-        ind = meta_matchLen.loc[meta_matchLen.orig_strain == dup_s].sort_values(by="matchlength", ascending=False).index[0]
-        meta_matchLen.drop(ind, inplace=True)
-    if dup_strains:
-        print("\n{} sequences had different accession numbers but the same strain name.".format(len(dup_strains)))
-        print("This is the same sample, sequenced multiple times and submitted under different accessions.")
-        print("The longest match will be taken and others discarded.")
-        print("Duplicates have been written out to {}".format(args.dup_file))
+        dups2 = meta_matchLen.duplicated(subset="orig_strain", keep="first")
+        dup_strains = list(meta_matchLen.loc[dups2,'orig_strain'])
+        for dup_s in dup_strains:
+            #sort by matchlength (longest at top), take first
+            ind = meta_matchLen.loc[meta_matchLen.orig_strain == dup_s].sort_values(by="matchlength", ascending=False).index[0]
+            meta_matchLen.drop(ind, inplace=True)
+        if dup_strains:
+            print("\n{} sequences had different accession numbers but the same strain name.".format(len(dup_strains)))
+            print("This is the same sample, sequenced multiple times and submitted under different accessions.")
+            print("The longest match will be taken and others discarded.")
+            print("Duplicates have been written out to {}".format(args.dup_file))
 
     over700 = len(meta_matchLen.loc[meta_matchLen.matchlength >= 700, "strain"])
     under700 = len(meta_matchLen.loc[meta_matchLen.matchlength < 700, "strain"])
@@ -113,6 +139,6 @@ if __name__ == '__main__':
     #remove the added qseqid column
     meta_matchLen = meta_matchLen.drop("qseqid", axis=1)
     #write out only those we are taking
-    meta_matchLen.to_csv(args.out_meta, sep='\t', index=False)
+    meta_matchLen.to_csv(args.out_meta, sep=sepr, index=False)
 
 
